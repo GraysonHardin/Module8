09797cddb8e79af0f5b5a21f433856751559cae4
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-19 11:29:16 -0500

    Tests pass, added docstring.

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -1,6 +1,16 @@
+"""
+Program: dict_membership.py
+Author: Grayson Hardin
+Last date modified: 10/19/2020
+"""
+
+
 def in_dict(values, search_term):
-    pass
+    results = search_term in values
+    return results
 
 
 if __name__ == '__main__':
-   in_dict()
+    print(in_dict(values={'A': 90, 'B': 80, 'C': 70, 'D': 60, 'F': 0}, search_term='A'))
+    print(in_dict(values={'A': 90, 'B': 80, 'C': 70, 'D': 60, 'F': 0}, search_term='G'))
+
