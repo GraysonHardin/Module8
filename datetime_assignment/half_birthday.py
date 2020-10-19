0d17f578317c1b4a3e61b0d48411e533285d4c76
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-19 11:47:54 -0500

    Test passes, docstring included, code functions as intended.

--- datetime_assignment/half_birthday.py
+++ datetime_assignment/half_birthday.py
@@ -1,12 +1,21 @@
+"""
+Program: half_birthday.py
+Author: Grayson Hardin
+Last date modified: 10/19/2020
+
+Program returns the date for half birthday.
+"""
+
+
 import datetime
 
 
 def half_birthday(birthday):
-    pass
+    return birthday + datetime.timedelta(days=365 / 2)  # This will calculate the date
 
 
 def main():
-    new_half_birthdate = half_birthday(birthday=datetime.date(2020, 7, 15))
+    new_half_birthdate = half_birthday(birthday=datetime.date(2020, 7, 15))  # Function called and assigned last birthday.
     print(new_half_birthdate)
 
 
