55d4f54a5a0dd6eea4176294a34a58141cba4199
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-19 10:30:14 -0500

    Added more code to production, tests pass, and included docstring.

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -1,6 +1,17 @@
+"""
+Program: set_membership.py
+Author: Grayson Hardin
+Last date modified: 10/19/2020
+
+The program is designed to accept a list of values, search for an item, and then print to the console whether the output is true or false.
+"""
+
+
 def in_set(values, search_term):
-    pass
+    results = search_term in values  # This will take search_term and locate whether it is within the stored values.
+    return results
 
 
 if __name__ == '__main__':
-    in_set()
+    print(in_set(values={1, 2, 3, 4, 5}, search_term=5))  # Both lines 7 and 8 are examples that can be modified.
+    print(in_set(values={1, 2, 3, 4, 5}, search_term=6))
